a3acc4827443889eb1e1d843ed3076745ec0e679
Accord99 <114819430+Accord99@users.noreply.github.com>
2022-10-26 13:29:46 +0300

    решили 2 задачу

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine($"Первое число больше второго, и оно равно {numberA}");
+}
+else if(numberB > numberA)
+{
+    Console.WriteLine($"Второе число больше первого, и оно равно {numberB}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
+
+
+
+
+
+
