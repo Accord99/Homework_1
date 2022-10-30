253016cd81a62e60dd1424a949fa902e8f21f793
Accord99 <114819430+Accord99@users.noreply.github.com>
2022-10-30 11:34:03 +0300

    доделали задание

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,5 +1,5 @@
 ﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
+Console.Clear();
 Console.WriteLine("Введите положительное число: ");
 int number = int.Parse(Console.ReadLine());
 int count = 1;
